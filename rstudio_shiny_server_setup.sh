eef88d1126aadf8df12cfde299ec2101d8dbe693
Ubuntu <ubuntu@ip-172-31-76-29.ec2.internal>
2020-08-03 23:52:07 +0000

    reminder of shiny ram limits

--- rstudio_shiny_server_setup.sh
+++ rstudio_shiny_server_setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#install R
+# install R
 echo y | sudo apt-get install r-base
 
-#install RStudio-Server
+# install RStudio-Server
 sudo apt-get install gdebi-core
 sudo wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1056-amd64.deb
 echo y | sudo gdebi rstudio-server-1.3.1056-amd64.deb
 sudo rm rstudio-server-1.3.1056-amd64.deb
 
-#install shiny and shiny-server
+# install shiny and shiny-server
+# FYI: Shiny should be installed on a machine with >1GB memory   
 sudo su - \
 	-c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
 sudo wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.14.948-amd64.deb
