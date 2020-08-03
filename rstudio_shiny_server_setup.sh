c214a522ec9221cda5ddb37490598d6548ccee8e
NK <nkk36@users.noreply.github.com>
2020-08-03 17:37:10 -0400

    Create rstudio_shiny_server_setup.sh

---
+++ rstudio_shiny_server_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#install R
+sudo apt-get install r-base
+
+#install RStudio-Server
+sudo apt-get install gdebi-core
+sudo wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1056-amd64.deb
+sudo gdebi rstudio-server-1.3.1056-amd64.deb
+sudo rm rstudio-server-1.3.1056-amd64.deb
+
+#install shiny and shiny-server
+sudo su - -c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
+sudo wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.14.948-amd64.deb
+sudo gdebi shiny-server-1.5.14.948-amd64.deb
+sudo rm shiny-server-1.5.14.948-amd64.deb
